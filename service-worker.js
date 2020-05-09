2bb8455e42f2dc116c2138db421ad47881188002
angelynafaa <angelyna826@gmail.com>
2020-05-09 16:16:21 +0700

    menyimpan asset ke chace

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "firstpwa";
+var urlsToCache = [
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/pages/home.html",
+  "/pages/about.html",
+  "/pages/contact.html",
+  "/css/materialize.min.css",
+  "/js/materialize.min.js",
+  "/js/nav.js"
+];
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+      caches
+        .match(event.request, { cacheName: CACHE_NAME })
+        .then(function(response) {
+          if (response) {
+            console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+            return response;
+          }
+   
+          console.log(
+            "ServiceWorker: Memuat aset dari server: ",
+            event.request.url
+          );
+          return fetch(event.request);
+        })
+    );
+  });
